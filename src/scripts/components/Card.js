d3e83747c7cc9bb00ea663be3665fa951359a32d
Aleksey Vakht <AlekseyVakht@yandex.ru>
2023-05-01 12:18:05 +0300

    vvv

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -45,7 +45,6 @@ export class Card {
     this._cardImage.src = this._link;
     this._cardImage.alt = this._name;
     this._element.querySelector('.element__heading').textContent = this._name;
-    this._checkDeleteIcon();
     this._setEventListeners();
     return this._element;
   }
