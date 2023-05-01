db9096c0a30da24ea3471261cecf0d8c7237e9e9
Aleksey Vakht <AlekseyVakht@yandex.ru>
2023-05-01 10:54:41 +0300

    vvv

--- src/scripts/components/PopupWithConfirm.js
+++ src/scripts/components/PopupWithConfirm.js
@@ -4,7 +4,6 @@ export class PopupWithConfirm extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
     this._form = this._popup.querySelector('.popup__form');
-    this._handleCardDelete = handleCardDelete;
     this._submitBtn = this._popup.querySelector('.popup__submit-btn');
     this._submitBtnText = this._submitBtn.textContent;
   }
